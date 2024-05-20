9f902e040cdaddfa965eea7cd4f78dea73d66044
chomatz <chomatz@gmail.com>
2024-05-20 11:45:07 +0800

    added a script to install ansible-navigator on steamdeck

---
+++ bash/install_navigator_on_steamdeck.sh
@@ -0,0 +1,9 @@
+#!/bin/env/bash
+
+# install required packages
+sudo steamos-readonly disable
+sudo pacman -Syu fuse-overlayfs podman python-pip
+sudo steamos-readonly enable
+
+# install ansible-navigator
+python3 -m pip install ansible-navigator --user
