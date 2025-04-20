0f7488728d14ab47d8820fd0adbe51db24324ea6
chomatz <chomatz@gmail.com>
2025-04-20 17:16:47 +0800

    fix: ensure pacman keys are populated

--- bash/install_navigator_on_steamdeck.sh
+++ bash/install_navigator_on_steamdeck.sh
@@ -2,6 +2,9 @@
 
 # install required packages
 sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
 sudo pacman -Syu fuse-overlayfs podman python-pip
 sudo steamos-readonly enable
 
