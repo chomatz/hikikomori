939116f459b98e75587f7d6e01832554bda0926f
chomatz <chomatz@gmail.com>
2025-04-21 08:17:04 +0800

    redo: remove commands obsoleted by collections_path

--- bash/install_ansible_collections.sh
+++ bash/install_ansible_collections.sh
@@ -1,6 +1,2 @@
 #!/bin/env bash
-
-echo this script assumes that ~/.ansible/collections is empty
-sleep 3
 ansible-galaxy install -r collections/requirements.yml
-mv ~/.ansible/collections/ansible_collections collections/.
